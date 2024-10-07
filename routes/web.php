8bed6310d088a61d84ef4d08752e5c83d9dee0f3
Montythehobo <andrewbannister.work@gmail.com>
2024-10-07 16:14:10 +0100

    change route to insert where you want

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\JsonDataController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', JsonDataController::formatData('C:\Users\Monty Clementine\AppData\Roaming\JetBrains\PhpStorm2024.2\scratches\scratch_1.json'));
+Route::get('/', JsonDataController::formatData(/* Enter File Here */));
